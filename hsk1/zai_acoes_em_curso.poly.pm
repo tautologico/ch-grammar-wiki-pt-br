cee517fbf77393b55be28c76e02794b60becf8c4
Andrei Formiga <archimedes_siracusa@hotmail.com>
2020-01-10 17:58:04 -0300

    artigo zai

---
+++ hsk1/zai_acoes_em_curso.poly.pm
@@ -0,0 +1,69 @@
+#lang pollen
+
+◊(define-meta title "Expressando ações em curso com 在")
+◊(define-meta tags ('gramatica 'hsk1 'a2 'adverbios))
+
+在 (zài) e 正在 (zhèngzài) podem ser utilizados como ◊ref{verbos auxiliares} para sinalizar que uma ação está em curso ou em progresso. Isso muitas vezes equivale ao *presente contínuo* em português.
+
+Você pode utilizar 正在 (zhèngzài) ao invés de apenas 在 (zài) para enfatizar um pouco mais que a ação está acontecendo **agora mesmo**.
+
+◊section{Estrutura}
+
+◊grammarstruct{Suj. + 在 + Verbo + Obj.}
+
+ou
+
+◊grammarstruct{Suj. + 正在 + Verbo + Obj.}
+
+◊section{Exemplos}
+
+◊example{ ◊zh{她在看书。}
+          ◊py{Tā zài kànshū.}
+          ◊pt{Ela está lendo.}
+        }
+
+◊example{ ◊zh{妈妈在打电话。}
+          ◊py{Māma zài dǎ diànhuà.}
+          ◊pt{Mamãe está falando ao telefone.}
+        }
+
+◊example{ ◊zh{}
+          ◊py{}
+          ◊pt{}
+        }
+-
+◊example{ ◊zh{}
+          ◊py{}
+          ◊pt{}
+        }
+-
+◊example{ ◊zh{}
+          ◊py{}
+          ◊pt{}
+        }
+-
+◊example{ ◊zh{}
+          ◊py{}
+          ◊pt{}
+        }
+-
+◊example{ ◊zh{}
+          ◊py{}
+          ◊pt{}
+        }
+-
+◊example{ ◊zh{}
+          ◊py{}
+          ◊pt{}
+        }
+-
+◊example{ ◊zh{}
+          ◊py{}
+          ◊pt{}
+        }
+-
+◊example{ ◊zh{}
+          ◊py{}
+          ◊pt{}
+        }
+-
