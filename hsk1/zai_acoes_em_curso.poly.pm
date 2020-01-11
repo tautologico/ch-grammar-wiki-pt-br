82ca066202fa59e3089d3c109c8760d05a64dd4e
Andrei Formiga <archimedes_siracusa@hotmail.com>
2020-01-11 00:33:09 -0300

    finish

--- hsk1/zai_acoes_em_curso.poly.pm
+++ hsk1/zai_acoes_em_curso.poly.pm
@@ -27,43 +27,51 @@ ou
           ◊pt{Mamãe está falando ao telefone.}
         }
 
-◊example{ ◊zh{}
-          ◊py{}
-          ◊pt{}
+◊example{ ◊zh{谁在里面洗澡。}
+          ◊py{Shéi zài lǐmiàn xǐzǎo?}
+          ◊pt{Quem está tomando banho ali?}
         }
--
-◊example{ ◊zh{}
-          ◊py{}
-          ◊pt{}
+
+◊example{ ◊zh{阿姨正在打扫我们的房间。}
+          ◊py{Āyí zhèngzài dǎsǎo wǒmen de fángjiān.}
+          ◊pt{A faxineira está limpando o nosso quarto neste momento.}
         }
--
-◊example{ ◊zh{}
-          ◊py{}
-          ◊pt{}
+
+◊example{ ◊zh{昨天晚上七点，我们在吃饭。}
+          ◊py{Zuótiān wǎnshang qīdiǎn, wǒmen zài chīfàn.}
+          ◊pt{Ontem às 7 da noite estávamos jantando.}
         }
--
-◊example{ ◊zh{}
-          ◊py{}
-          ◊pt{}
+
+◊example{ ◊zh{老板在开会，没有时间你。}
+          ◊py{Lǎobǎn zài kāihuì, méiyǒu shíjiān jiàn nǐ.}
+          ◊pt{O chefe está em reunião, não tem tempo para ver você agora.}
         }
--
-◊example{ ◊zh{}
-          ◊py{}
-          ◊pt{}
+
+◊example{ ◊zh{我现在在上班，不方便离开。}
+          ◊py{Wǒ xiànzài zài shàngbān, bù fāngbiàn líkāi.}
+          ◊pt{Estou trabalhando agora, fica difícil sair.}
         }
--
-◊example{ ◊zh{}
-          ◊py{}
-          ◊pt{}
+
+◊example{ ◊zh{我们正在上课，请你等一会儿。}
+          ◊py{Wǒmen zhèngzài shàngkè, qǐng nǐ děng yīhuìr.}
+          ◊pt{Estamos em aula agora, espere um pouco, por favor.}
         }
--
-◊example{ ◊zh{}
-          ◊py{}
-          ◊pt{}
+
+◊example{ ◊zh{你正在开车，不可以玩手机。}
+          ◊py{Nǐ zhèngzài kāichē, bù kěyǐ wán shǒujī.}
+          ◊pt{Você está dirigindo neste momento, não pode usar o celular.}
         }
--
-◊example{ ◊zh{}
-          ◊py{}
-          ◊pt{}
+
+◊example{ ◊zh{你给我打电话的时候，我正在跟朋友打游戏。}
+          ◊py{Nǐ gěi wǒ dǎ diànhuà de shíhou, wǒ zhèngzài gēn péngyou dǎ yóuxì.}
+          ◊pt{Na hora que você ligou eu estava jogando videogame com os amigos.}
         }
--
+
+◊section{Veja também}
+
+◊ref{Partícula de aspecto 着}
+◊ref{Expressando ações em curso (forma completa)}
+
+◊section{Referências}
+
+Veja o site no wiki: ◊url{https://resources.allsetlearning.com/chinese/grammar/Expressing_actions_in_progress_with_%22zai%22}.
